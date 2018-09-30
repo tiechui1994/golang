17d3d92b0031c9c668acd567b1e7766ee9de4856
tiechui1994 <2904951429@qq.com>
2018-09-30 13:58:17 +0800

    add new content about log

--- logging/log.go
+++ logging/log.go
@@ -12,14 +12,14 @@ import (
 	"log"
 )
 
-// import "github.com/astaxie/beego/logs"
+//import "github.com/astaxie/beego/logs"
 //
 //	log := NewLogger(10000)
 //	log.SetLogger("console", "")
 //
 //	> the first params stand for how many channel
 //
-// Use it like this:
+//Use it like this:
 //
 //	log.Trace("trace")
 //	log.Info("info")
@@ -27,8 +27,9 @@ import (
 //	log.Debug("debug")
 //	log.Critical("critical")
 //
+
 const (
-	LevelEmergency     = iota
+	LevelEmergency = iota
 	LevelAlert
 	LevelCritical
 	LevelError
@@ -38,8 +39,7 @@ const (
 	LevelDebug
 )
 
-// levelLogLogger is defined to implement log.Logger
-// the real log level will be LevelEmergency
+// 作为参考设置日志级别
 const levelLoggerImpl = -1
 
 // 官方实现的Log
@@ -55,14 +55,17 @@ const (
 	AdapterAliLS     = "alils"
 )
 
-// Legacy log level constants to ensure backwards compatibility.
+// 常见的Log级别 -> 定义的Log级别(实际设计当中可选)
 const (
 	LevelInfo  = LevelInformational
 	LevelTrace = LevelDebug
 	LevelWarn  = LevelWarning
 )
 
+// 注册函数 -> 初始化调用.
+// 扩展性考虑
 type newLoggerFunc func() Logger
+
 // Log的顶层接口
 type Logger interface {
 	Init(config string) error
@@ -71,10 +74,11 @@ type Logger interface {
 	Flush()
 }
 
-var adapters = make(map[string]newLoggerFunc) // log形式
+// 保存所有实现的Logger的引擎
+var adapters = make(map[string]newLoggerFunc)
 var levelPrefix = [LevelDebug + 1]string{"[M] ", "[A] ", "[C] ", "[E] ", "[W] ", "[N] ", "[I] ", "[D] "}
 
-// 注册的方式增加Logger的实现
+// 注册的方式增加Logger的实现, 主要目的是为了扩展
 func Register(name string, log newLoggerFunc) {
 	if log == nil {
 		panic("logs: Register provide is nil")
@@ -85,54 +89,64 @@ func Register(name string, log newLoggerFunc) {
 	adapters[name] = log
 }
 
-// 消息
+//--------------------------------------------------------------------------------------------------------------------
+
+// 对Logger进一步封装, 目的是为了每个实现的Logger进行命名, 方便操作
 type nameLogger struct {
 	Logger
-	name string  //adapter的名称
+	name string //adapter的名称
 }
 
-// Log消息定义
+// Log消息定义, 实际转发的参数封装
 type logMsg struct {
 	level int
 	msg   string
 	when  time.Time
 }
 
-// 默认Log的实现
+// Logger的调度器, 管理所有用户添加的Logger
+// 注意: 用户添加的Logger != 注册的Logger
 type BeeLogger struct {
-	lock                sync.Mutex
-	level               int
-	init                bool
-	enableFuncCallDepth bool
-	loggerFuncCallDepth int
-	asynchronous        bool
-	prefix              string
-	msgChanLen          int64
-	msgChan             chan *logMsg
-	signalChan          chan string
-	wg                  sync.WaitGroup
-	outputs             []*nameLogger
+	lock                sync.Mutex    // 锁定init只能是一次, 锁定启动异步日志, 锁定添加Logger引擎顺序
+	level               int           // Logger引擎定义的Log级别(可配置)
+	init                bool          // 初始化控制, 只能初始化一次
+	enableFuncCallDepth bool          // 是否记录log产生的位置
+	loggerFuncCallDepth int           // 当记录log产生的位置, 记录位置的深度
+	prefix              string        // Logger引擎自定义独特的标识符(可配置)
+	outputs             []*nameLogger // 用户添加的Logger引擎
+
+	asynchronous bool
+	wg           sync.WaitGroup
+	msgChanLen   int64        // 日志管道长度
+	msgChan      chan *logMsg // 消息管道, 进行异步转发消息 -> msgChanLen
+	signalChan   chan string  // 信号, 注意负责日志的flush和destroy -> 1
 }
 
-const defaultAsyncMsgLen = 1e3 //异步log的通道的长度
+//默认异步log的通道的长度
+const defaultAsyncMsgLen = 1e3
+
 // 一个sync.Pool对象就是一组临时对象的集合.Pool是协程安全的. Pool用于存储那些被分配了但是没有被使用,而未来可能会使用的值,以减小垃圾回收的压力
 var logMsgPool *sync.Pool
 
-// 返回一个Logger实例
+// 构建一个Logger调度器
 func NewLogger(channelLens ...int64) *BeeLogger {
 	bl := new(BeeLogger)
 	bl.level = LevelDebug
 	bl.loggerFuncCallDepth = 2
+
+	// 只是指定了msgChanLen, 但是并没有实例化 msgChan(实例化了msgChan意味着异步日志)
 	bl.msgChanLen = append(channelLens, 0)[0]
 	if bl.msgChanLen <= 0 {
 		bl.msgChanLen = defaultAsyncMsgLen
 	}
+
+	// 控制异步日志的flush与close
 	bl.signalChan = make(chan string, 1)
-	bl.setLogger(AdapterConsole)
+	bl.setLogger(AdapterConsole) // 默认的会增加 AdapterConsole 引擎(控制台日志)
 	return bl
 }
 
-// 异步启动日志(默认是同步启动)
+// 启动异步日志(默认是同步日志), 任何时刻, 同时还可以设置日志队列长度
 func (bl *BeeLogger) Async(msgLen ...int64) *BeeLogger {
 	bl.lock.Lock()
 	defer bl.lock.Unlock()
@@ -143,40 +157,52 @@ func (bl *BeeLogger) Async(msgLen ...int64) *BeeLogger {
 	if len(msgLen) > 0 && msgLen[0] > 0 {
 		bl.msgChanLen = msgLen[0]
 	}
+
+	// 异步日志的标志
 	bl.msgChan = make(chan *logMsg, bl.msgChanLen)
+
 	// logMsg池, 专门产生logMsg对象
 	logMsgPool = &sync.Pool{
 		New: func() interface{} {
 			return &logMsg{}
 		},
 	}
-	bl.wg.Add(1)
+	bl.wg.Add(1) // 主线程
 	go bl.startLogger()
 	return bl
 }
 
-// 实例化并初始化一个Adapter, 这里的配置信息是Adapter的配置信息, 切记,切记
-func (bl *BeeLogger) setLogger(adapterName string, configs ...string) error {
-	config := append(configs, "{}")[0]
-	for _, l := range bl.outputs {
-		if l.name == adapterName {
-			return fmt.Errorf("logs: duplicate adaptername %q (you have set this logger before)", adapterName)
+// 异步日志: 循环从msgChan读取日志并进行分发
+func (bl *BeeLogger) startLogger() {
+	// 经典: 日志的flush和close
+	// flush -> 清空当前的msgChan的所有的日志, 协成空转
+	// close -> 清空当前的msgChan的所有的日志, 协成关闭
+	gameOver := false
+	for {
+		select {
+		case bm := <-bl.msgChan:
+			bl.writeToLoggers(bm.when, bm.msg, bm.level) // 异步分发操作 ^ _ ^
+			logMsgPool.Put(bm)
+		case sg := <-bl.signalChan:
+			// Now should only send "flush" or "close" to bl.signalChan
+			bl.flush()
+			if sg == "close" {
+				for _, l := range bl.outputs {
+					l.Destroy()
+				}
+				bl.outputs = nil
+				gameOver = true
+			}
+
+			// 这里协程不会退出,会继续空转, 但是主现成Wait()方法阻塞关闭
+			bl.wg.Done() // 发送一个信号,表示当前的工作完成
+		}
+
+		// 协程会关闭
+		if gameOver {
+			break
 		}
 	}
-
-	log, ok := adapters[adapterName]
-	if !ok {
-		return fmt.Errorf("logs: unknown adaptername %q (forgotten Register?)", adapterName)
-	}
-
-	lg := log() //创建一个引擎实例
-	err := lg.Init(config) // 初始化
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "logs.BeeLogger.SetLogger: "+err.Error())
-		return err
-	}
-	bl.outputs = append(bl.outputs, &nameLogger{name: adapterName, Logger: lg})
-	return nil
 }
 
 // 增加Adapter
@@ -190,11 +216,35 @@ func (bl *BeeLogger) SetLogger(adapterName string, configs ...string) error {
 	return bl.setLogger(adapterName, configs...)
 }
 
-// 删除Adapter
+// 实例化并初始化一个Adapter, 在这里进行日志引擎的初始化工作
+func (bl *BeeLogger) setLogger(adapterName string, configs ...string) error {
+	config := append(configs, "{}")[0]
+	for _, l := range bl.outputs {
+		if l.name == adapterName {
+			return fmt.Errorf("logs: duplicate adaptername %q (you have set this logger before)", adapterName)
+		}
+	}
+
+	log, ok := adapters[adapterName]
+	if !ok {
+		return fmt.Errorf("logs: unknown adaptername %q (forgotten Register?)", adapterName)
+	}
+
+	lg := log()            // 创建一个引擎实例
+	err := lg.Init(config) // 初始化
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "logs.BeeLogger.SetLogger: "+err.Error())
+		return err
+	}
+	bl.outputs = append(bl.outputs, &nameLogger{name: adapterName, Logger: lg})
+	return nil
+}
+
+// 删除指定日志引擎
 func (bl *BeeLogger) DelLogger(adapterName string) error {
 	bl.lock.Lock()
 	defer bl.lock.Unlock()
-	outputs := []*nameLogger{}
+	outputs := make([]*nameLogger, 0)
 	for _, lg := range bl.outputs {
 		if lg.name == adapterName {
 			lg.Destroy()
@@ -209,7 +259,9 @@ func (bl *BeeLogger) DelLogger(adapterName string) error {
 	return nil
 }
 
-// 日志分发
+// 日志分发操作:
+// 		同步分发,
+// 		异步分发, flush分发
 func (bl *BeeLogger) writeToLoggers(when time.Time, msg string, level int) {
 	for _, l := range bl.outputs {
 		err := l.WriteMsg(when, msg, level)
@@ -219,13 +271,14 @@ func (bl *BeeLogger) writeToLoggers(when time.Time, msg string, level int) {
 	}
 }
 
+// 写入:
 func (bl *BeeLogger) Write(p []byte) (n int, err error) {
 	if len(p) == 0 {
 		return 0, nil
 	}
 	// 写入日志总是在末尾添加'\n', 因此需要去掉末尾的'\n'
 	if p[len(p)-1] == '\n' {
-		p = p[0: len(p)-1]
+		p = p[0 : len(p)-1]
 	}
 	// set levelLoggerImpl to ensure all log message will be write out
 	err = bl.writeMsg(levelLoggerImpl, string(p))
@@ -235,8 +288,10 @@ func (bl *BeeLogger) Write(p []byte) (n int, err error) {
 	return 0, err
 }
 
-// logLevel是日志级别, msg是日志格式(v是日志对应格式内容)/日志内容
+// 写入日志:
+// 		logLevel是日志级别, msg是日志格式(v是日志对应格式内容)/日志内容
 func (bl *BeeLogger) writeMsg(logLevel int, msg string, v ...interface{}) error {
+	// 什么情况?
 	if !bl.init {
 		bl.lock.Lock()
 		bl.setLogger(AdapterConsole)
@@ -267,7 +322,7 @@ func (bl *BeeLogger) writeMsg(logLevel int, msg string, v ...interface{}) error 
 		msg = levelPrefix[logLevel] + msg // 增加系统设置的前缀
 	}
 
-	// 异步: 异步写入  同步:直接写
+	// 日志写入, 同步:分发  异步:管道->分发
 	if bl.asynchronous {
 		lm := logMsgPool.Get().(*logMsg) // 获取一个记录日志的对象
 		lm.level = logLevel
@@ -275,7 +330,7 @@ func (bl *BeeLogger) writeMsg(logLevel int, msg string, v ...interface{}) error 
 		lm.when = when
 		bl.msgChan <- lm // 发送到日志通道当中
 	} else {
-		bl.writeToLoggers(when, msg, logLevel)
+		bl.writeToLoggers(when, msg, logLevel) // 同步分发操作
 	}
 	return nil
 }
@@ -309,32 +364,6 @@ func (bl *BeeLogger) EnableFuncCallDepth(b bool) {
 // set prefix
 func (bl *BeeLogger) SetPrefix(s string) {
 	bl.prefix = s
-}
-
-// 异步方式: 启动logger的channel的读功能, 并开始写入日志
-func (bl *BeeLogger) startLogger() {
-	gameOver := false
-	for {
-		select {
-		case bm := <-bl.msgChan:
-			bl.writeToLoggers(bm.when, bm.msg, bm.level)
-			logMsgPool.Put(bm)
-		case sg := <-bl.signalChan:
-			// Now should only send "flush" or "close" to bl.signalChan
-			bl.flush()
-			if sg == "close" {
-				for _, l := range bl.outputs {
-					l.Destroy()
-				}
-				bl.outputs = nil
-				gameOver = true
-			}
-			bl.wg.Done()
-		}
-		if gameOver {
-			break
-		}
-	}
 }
 
 // 写日志
@@ -420,7 +449,7 @@ func (bl *BeeLogger) Trace(format string, v ...interface{}) {
 	bl.writeMsg(LevelDebug, format, v...)
 }
 
-// 刷新日志.(同步直接刷新, 异步发送信号进行刷新)
+// 刷新
 func (bl *BeeLogger) Flush() {
 	if bl.asynchronous {
 		bl.signalChan <- "flush"
@@ -428,16 +457,18 @@ func (bl *BeeLogger) Flush() {
 		bl.wg.Add(1)
 		return
 	}
+	// 同步: 刷新
 	bl.flush()
 }
 
-// 关闭日志.(同步和异步) flush所有的日志, 同时destroy所有的日志记录方式
+// 关闭
 func (bl *BeeLogger) Close() {
 	if bl.asynchronous {
 		bl.signalChan <- "close"
 		bl.wg.Wait()
 		close(bl.msgChan)
 	} else {
+		// 同步: 刷新 + 销毁
 		bl.flush()
 		for _, l := range bl.outputs {
 			l.Destroy()
@@ -447,12 +478,14 @@ func (bl *BeeLogger) Close() {
 	close(bl.signalChan)
 }
 
-// 重置日志记录方式
+// 重置: 销毁所有日志引擎, 依旧还可以添加新的日志引擎
 func (bl *BeeLogger) Reset() {
 	bl.Flush()
 	for _, l := range bl.outputs {
 		l.Destroy()
 	}
+
+	// 将outputs置空, 在这里等价于: bl.outputs = []*nameLogger{}, 地址不会发生改变
 	bl.outputs = nil
 }
 
@@ -462,8 +495,8 @@ func (bl *BeeLogger) flush() {
 		for {
 			if len(bl.msgChan) > 0 {
 				bm := <-bl.msgChan
-				bl.writeToLoggers(bm.when, bm.msg, bm.level)
-				logMsgPool.Put(bm) //回收对象
+				bl.writeToLoggers(bm.when, bm.msg, bm.level) // flush操作
+				logMsgPool.Put(bm)                           //回收对象
 				continue
 			}
 			break
@@ -474,7 +507,7 @@ func (bl *BeeLogger) flush() {
 	}
 }
 
-
+//--------------------------------------------------------------------------------------------------------------------
 
 // 初始化一个日志对象
 var beeLogger = NewLogger()
