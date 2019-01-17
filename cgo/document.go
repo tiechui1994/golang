59794847a5f5350ee130ab8e05751455c28fa3fd
tiechui1994 <2904951429@qq.com>
2019-01-17 09:53:46 +0800

    add cgo type convert

--- cgo/document.go
+++ cgo/document.go
@@ -303,7 +303,6 @@ typedef struct {
 } GoSlice;
 ```
 
-
 案例:
 
 ```
@@ -320,5 +319,197 @@ CGO生成的 _cgo_export.h 头文件会包含以下的函数声明:
 extern void helloString(GoString p0);
 extern void helloSlice(GoSlice p0);
 ```
+
+========================================================================================================================
+
+结构体、联合、枚举类型:
+
+C语言的结构体, 联合, 枚举类型不能作为匿名成员被嵌入到Go语言的结构体内. 在Go当中, 可以通过
+C.struct_xxx 来访问C语言中定义的 struct xxx 结构体类型.
+
+结构体的内存布局按照C语言的通用对齐规则,  在32位Go语言环境C语言结构体按照32位对齐规则, 在
+64位Go语言环境C语言结构体也按照64位对齐规则. 对于指定了特殊对齐规则的结构体, 无法在CGO中
+访问.
+
+```
+//struct A {
+//    int i;
+//    float f;
+//};
+import "C"
+import "fmt"
+
+func main() {
+	var a C.struct_A
+	fmt.Println(a.i)
+	fmt.Println(a.f)
+}
+```
+
+注: 	1.当结构体当中存在以Go关键字命名的成员, 则需要通过在成员名开头添加下划线来访问.
+例如, struct A { int type; }; 访问type变量的方式是 a._type, a是A生成的对象.
+	2.C语言结构体中"位字段"对应的成员无法在Go语言中被访问, 如果需要操作位字段成员, 需
+要通过在C语言中定义辅助函数来完成. 对于"零长数值的成员", 无法在Go语言中直接访问数组的
+元素, 但其中零长的数组成员所在的位置的偏移量可以通过 unsafe.Offset(a.arr) 来访问.
+
+```
+// struct A {
+//     int size:10; // todo: 位字段, 无法访问
+//     float arr[]; // todo: 零长的数组, 无法访问
+// }
+import "C"
+import "fmt"
+
+func main() {
+	var a C.struct_A
+	fmt.Println(a.size) // 错误
+	fmt.Println(a.arr)  // 错误
+}
+```
+
+	3. C语言中, 无法直接访问Go语言定义的结构体类型.
+
+
+对于联合类型, 通过C.union_xxx 来访问C语言中定义的 union xxx 类型. 但是在Go当中不支持C语言
+的联合类型, 它们会被转换为对于大小的字节数组.
+
+```
+//#include <stdint.h>
+//
+//union B1 {
+//    int i;
+//    float f;
+//};
+//
+//union B2 {
+//    int8_t i8;
+//    int64_t i64;
+//};
+import "C"
+import "fmt"
+
+func main() {
+	var b1 C.union_B1;
+	fmt.Printf("%T\n", b1) // [4]uint8
+
+	var b2 C.union_B2;
+	fmt.Printf("%T\n", b2) // [8]uint8
+}
+```
+
+如果需要操作C语言的联合类型变量, 有三种方式: 第一种是在C语言当中定义辅助函数; 第二种是通过Go语言
+的"encoding/binary" 手工解码成员(需要注意大端小端问题); 第三种是使用unsafe包强制转为对应类型(
+性能最好的方式)
+
+案例: unsafe强制转换
+```
+//#include <stdint.h>
+//union B {
+//   int   i;
+//   float f;
+//};
+import "C"
+import "fmt"
+
+func main() {
+	var b C.union_B
+	fmt.Println("b.i:", *(*C.int)(unsafe.Pointer(&b)))
+	fmt.Println("b.f:", *(*C.float)(unsafe.Pointer(&b)))
+}
+```
+
+对于枚举类型, 可以通过C.enum_xxx来访问C语言中定义的enum xxx 结构体类型.
+
+========================================================================================================================
+
+数组, 切片和字符串:
+
+CGO辅助函数:
+```
+// Go string to C string
+// C字符串使用malloc在C堆中分配, 调用者有责任安排释放它. 例如通过调用 C.free(如果需要C.free,请务必包含stdlib.h).
+func C.CString(string) *C.char
+
+// Go []byte slice to C array
+// C数组使用malloc在C堆中分配. 调用者有责任安排释放它. 例如通过调用C.free(如果需要C.free,请务必包含stdlib.h).
+func C.CBytes([]byte) unsafe.Pointer
+
+// C string to Go string
+func C.GoString(*C.char) string
+
+// C data with explicit length to Go string
+func C.GoStringN(*C.char, C.int) string
+
+// C data with explicit length to Go []byte
+func C.GoBytes(unsafe.Pointer, C.int) []byte
+```
+
+C.CString针对输入的Go字符串, 克隆一个C语言格式的字符串; 返回的字符串由C语言的malloc函数分配,
+不使用时需要通过C语言的free函数释放.
+
+C.CBytes函数的功能和C.CString类似,用于从输入的Go语言字节切片克隆一个C语言版本的字节数组, 同样
+返回的数组需要在合适的时候释放.
+
+C.GoString用于将从NULL结尾的C语言字符串克隆一个Go语言字符串.
+
+C.GoStringN是另一个字符数组克隆函数.
+
+C.GoBytes用于从C语言数组,克隆一个Go语言字节切片.
+
+该组辅助函数都是以克隆的方式运行. 当Go语言字符串和切片向C语言转换时, 克隆的内存由C语言的malloc函数分配,
+最终可以通过free函数释放. 当C语言字符串或数组向Go语言转换时, 克隆的内存由Go语言分配管理. 通过该组转换函
+数, 转换前和转换后的内存依然在各自的语言环境中, 它们并没有跨越Go语言和C语言. 克隆方式实现转换的优点是接口
+和内存管理都很简单, 缺点是克隆需要分配新的内存和复制操作都会导致额外的开销.
+
+
+手动转换:
+
+reflec包的字符串和切片的定义:
+```
+type StringHeader struct {
+    Data uintptr
+    Len  int
+}
+
+type SliceHeader struct {
+    Data uintptr
+    Len  int
+    Cap  int
+}
+```
+
+使用:
+```
+//static char arr[10];
+//static char *s = "Hello";
+import "C"
+import (
+	"fmt"
+	"reflect"
+	"unsafe"
+)
+
+func main() {
+	// 通过 reflect.SliceHeader 转换
+	var arr0 []byte
+	var arr0Hdr = (*reflect.SliceHeader)(unsafe.Pointer(&arr0))
+	arr0Hdr.Data = uintptr(unsafe.Pointer(&C.arr[0]))
+	arr0Hdr.Len = 10
+	arr0Hdr.Cap = 10
+
+	// 通过切片语法转换
+	arr1 := (*[31]byte)(unsafe.Pointer(&C.arr[0]))[:10:10]
+
+	var s0 string
+	var s0Hdr = (*reflect.StringHeader)(unsafe.Pointer(&s0))
+	s0Hdr.Data = uintptr(unsafe.Pointer(C.s))
+	s0Hdr.Len = int(C.strlen(C.s))
+
+	sLen := int(C.strlen(C.s))
+	s1 := string((*[31]byte)(unsafe.Pointer(&C.s[0]))[:sLen:sLen])
+}
+```
+
+注: 这里牵扯到CGO内存模型, 相关问题其他模块解析.
 
 ***********************************************************************************************************************/
