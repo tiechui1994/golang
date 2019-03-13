88c2c76630b662e6f2a268cf538fd50b42a16a86
tiechui1994 <2904951429@qq.com>
2019-03-13 19:31:18 +0800

    add util function to validate file header

--- concurrent/concurrent.go
+++ concurrent/concurrent.go
@@ -6,8 +6,38 @@ import (
 
 /*
 并发控制的模式:
-	方式一: 使用channel
-	方式二: 使用WaitGroup
+
+方式一: 使用channel.
+
+	done := make(chan bool)
+	defer close(done)
+	for {
+		// 并发执行
+		go func(word string) {
+			time.Sleep(1 * time.Second)
+			fmt.Println(word)
+			done <- true
+		}(word)
+	}
+
+	<-done // 阻塞执行
+
+
+方式二: 使用WaitGroup.
+场景: 程序中需要并发, 需要创建多个goroutine, 并且一定要等这些并发全部完成后才继续接下来的程序执行,
+WaitGroup的特点是Wait()可以用来阻塞直到队列中的所有任务都完成时才解除阻塞, 而不需要sleep一个固定
+的时间来等待.但是其缺点是无法指定固定的goroutine数目.
+
+	var wg sync.WaitGroup
+	for {
+		wg.Add(1)
+		go func(word string) {
+			time.Sleep(1 * time.Second)
+			defer wg.Done()
+			fmt.Println(word)
+		}(word)
+	}
+	wg.Wait() // 阻塞
 */
 
 type ChannelGroup struct {
