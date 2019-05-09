8b62ccbbc1a005870046e84c31190a33f6432081
tiechui1994 <2904951429@qq.com>
2019-05-09 19:02:46 +0800

    update go map

--- details/map.go
+++ details/map.go
@@ -1,3 +1,18 @@
 package details
 
+/*
+map 的 key 类型: map的key的类型只要支持 `==` 和 `!=` 操作即可, 即Comparabale
+	- bool
+	- number
+	- string
+	- pointer
+	- channel
+	- interface
+	- struct
+	- array (Type 是上述类型)
 
+map的 key 不能是以下类型:
+	- slice
+	- map
+	- function
+**/
