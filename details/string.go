d960ce1c316d7546448c244d6a37efb7cbeee03e
tiechui1994 <2904951429@qq.com>
2019-05-09 14:46:14 +0800

    add new conent for detail go

--- details/string.go
+++ details/string.go
@@ -19,10 +19,27 @@ func StringValue() {
 
 // 字符串长度
 // 内置的 len() 函数返回的是 byte 的数量.
-// 对于runecode 在 0-127 这之间的字符的
+// 对于 unicode 在 0-127 这之间的 char 的 byte 是1, 128以上的 char 的 byte 大于1
+// utf8.RuneCountInString() 函数返回的是 rune 的数量. 即 unicode 的数量
 
 func StringLen() {
-	fmt.Println("\x99")
-	fmt.Println(len("，"))
-	fmt.Println("\xff", len("\x10"), rune(12))
+	char := "♥"
+	fmt.Println(char, "rune", utf8.RuneCountInString(char))
+	fmt.Println(char, "rune len", len([]rune(char)))
+	fmt.Println(char, "len", len(char))
+}
+
+// 字符和数字之间的转换
+// char -> int            int(char)
+// int -> char -> string  string(int)
+func Convert() {
+	for i := 0; i < 16535; i++ {
+		fmt.Printf("%s	", string(i))
+		if i%30 == 0 {
+			fmt.Println()
+		}
+	}
+	fmt.Println()
+
+	fmt.Println(int('✓'), int('✔'), int('✕'))
 }
